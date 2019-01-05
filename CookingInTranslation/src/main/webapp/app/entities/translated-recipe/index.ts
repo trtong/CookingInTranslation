501c3f306a8ec4fa2d386f9d9200738e8f7c4392
Trinh Tong <trt.tong@gmail.com>
2019-01-05 13:34:16 -0500

    Revert "import jdl"
    This reverts commit d8bb2906515b66153781e4d5e6fd3e14966dfe88.

--- CookingInTranslation/src/main/webapp/app/entities/translated-recipe/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './translated-recipe.service';
-export * from './translated-recipe-update.component';
-export * from './translated-recipe-delete-dialog.component';
-export * from './translated-recipe-detail.component';
-export * from './translated-recipe.component';
-export * from './translated-recipe.route';

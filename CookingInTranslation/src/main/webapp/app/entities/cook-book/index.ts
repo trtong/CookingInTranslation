acab0353a8e2e22c530a69560906a2a822a3b656
Trinh Tong <trt.tong@gmail.com>
2019-01-05 12:37:11 -0500

    Revert "Import JDL file"
    This reverts commit c716380d17f98121be9238acceba3905fb6d6580.

--- CookingInTranslation/src/main/webapp/app/entities/cook-book/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './cook-book.service';
-export * from './cook-book-update.component';
-export * from './cook-book-delete-dialog.component';
-export * from './cook-book-detail.component';
-export * from './cook-book.component';
-export * from './cook-book.route';
